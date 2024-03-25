35a7af440c26d01ae96cc3756680e9b4711027cb
Jonathan Frey <jonathanpaulfrey@gmail.com>
2024-03-25 16:29:08 +0100

    add define_casadi_symbolic

--- +vdx/Vector.m
+++ +vdx/Vector.m
@@ -273,3 +273,27 @@ classdef Vector < handle &...
         end
     end
 end
+
+
+function sym = define_casadi_symbolic(type, name, dims, sparsity)
+    import casadi.*
+    if nargin < 3
+        dims = 1;
+    end
+
+    if ~exist('sparsity', 'var')
+        if size(dims, 2) == 1
+            sparsity = Sparsity.dense([dims,1]);
+        else
+            sparsity = Sparsity.dense(dims);
+        end
+    end
+
+    if strcmp(type, 'casadi.SX') || strcmp(type, 'SX')
+        sym = SX.sym(name, sparsity);
+    elseif strcmp(type, 'casadi.MX')  || strcmp(type, 'MX')
+        sym = MX.sym(name, sparsity);
+    else
+        error('Type must be MX or SX.')
+    end
+end
